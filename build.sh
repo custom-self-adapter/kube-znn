ea8b129bf7d493ba86ef919f285d8104103cbb0c
Humberto Fraga <humbertofraga@gmail.com>
2025-07-19 15:15:38 -0300

    Updates build.sh to use the lab's registry

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in '20k' '100k' '200k' '400k' '600k' '800k' 'text'; do
-    docker build --build-arg BUILDTIME_FIDELITY=$i -t cmendes/znn:$i .
-    docker push cmendes/znn:$i
+for i in '20k' '100k' '200k' '400k' '600k' '800k'; do
+    docker build --build-arg BUILDTIME_FIDELITY=$i -t registry.k8s.lab/znn:$i .
+    docker push registry.k8s.lab/znn:$i
 done
