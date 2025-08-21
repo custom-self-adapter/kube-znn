5dd8d06b6ff4c67de979b0f5527ec876b2aee306
Humberto Fraga <humbertofraga@gmail.com>
2025-08-21 19:15:59 -0300

    Allows to customize the image name

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+REPOSITORY=registry.k8s.lab/znn
+
+if [ ! -z $1 ]
+then
+    REPOSITORY=$1
+fi
+
 for i in '20k' '100k' '200k' '400k' '600k' '800k'; do
-    docker build --build-arg BUILDTIME_FIDELITY=$i -t registry.k8s.lab/znn:$i .
-    docker push registry.k8s.lab/znn:$i
+    docker build --build-arg BUILDTIME_FIDELITY=$i -t $REPOSITORY:$i .
+    docker push $REPOSITORY:$i
 done
